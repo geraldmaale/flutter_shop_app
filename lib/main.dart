08e96a836cdfb009c83a22ac5352abb9e632b602
Gerald Maale <geraldmaale@hotmail.com>
2023-07-17 10:12:45 +0800

    Added validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'views/screens/auth/login_screen.dart';
 
 void main() {
@@ -13,8 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Shop App',
-        home: LoginScreen());
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Shop App',
+      home: LoginScreen(),
+    );
   }
 }
--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -3,94 +3,118 @@ import 'package:flutter/material.dart';
 import 'register_screen.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key}) : super(key: key);
+
+  static final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  // properties for fields
+  static late String _email;
+  static late String _password;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(
-          left: 20,
-          right: 20,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Login Account",
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 3,
-              ),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Email Address',
-                hintText: "Enter email address",
-                prefixIcon: Icon(
-                  Icons.email,
-                  color: Colors.blue,
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Login Account",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 3,
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                hintText: "Enter password",
-                prefixIcon: Icon(
-                  Icons.lock_person,
-                  color: Colors.blue,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              height: 50,
-              width: MediaQuery.of(context).size.width - 40,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(
-                  16,
-                ),
-              ),
-              child: const Center(
-                child: Text(
-                  "Login",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2,
+              TextFormField(
+                validator: (value) => value!.isEmpty ? 'Enter email' : null,
+                onChanged: (value) => _email = value,
+                decoration: const InputDecoration(
+                  labelText: 'Email Address',
+                  hintText: "Enter email address",
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Colors.blue,
                   ),
-                  textAlign: TextAlign.center,
                 ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  child: const Text("Forgot Password?"),
+              const SizedBox(
+                height: 8,
+              ),
+              TextFormField(
+                validator: (value) => value!.isEmpty ? 'Enter password' : null,
+                onChanged: (value) => _password = value,
+                obscureText: true,
+                obscuringCharacter: '*',
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  hintText: "Enter password",
+                  prefixIcon: Icon(
+                    Icons.lock_person,
+                    color: Colors.blue,
+                  ),
                 ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const RegisterScreen()));
-                  },
-                  child: const Text("Need an Account?"),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              InkWell(
+                onTap: () {
+                  if (_formKey.currentState!.validate()) {
+                    //todo: login user
+                    print("Email: $_email");
+                    print("Password: $_password");
+                  }
+                },
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(
+                      16,
+                    ),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 2,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text("Forgot Password?"),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RegisterScreen()));
+                    },
+                    child: const Text("Need an Account?"),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
